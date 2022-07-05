e21483a6a037ed1355aff3797b3a855f490ae978
Marlus Santiago <santiago.marlus@gmail.com>
2022-07-05 11:51:46 -0300

    camera new script

--- index.js
+++ index.js
@@ -1,68 +1,24 @@
-function listDevices(){
-  
-  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-    console.log("enumerateDevices() not supported.");
-    $('#log').html(`enumerateDevices() not supported.`)
-    return false;
-  }
-  
-  // List cameras and microphones.
-  
-  navigator.mediaDevices.enumerateDevices()
-  .then(function(devices) {
+async function initializeCamera() {
+  stopVideoStream();
+  constraints.video.facingMode = useFrontCamera ? "user" : "environment";
 
-    $('#log').html(`devices: \n\n${JSON.stringify(devices)}`)
-
-    devices.forEach(element => {
-      let cam = element.label.split(' ')
-      if (cam[3] === 'front'){
-        $('#log').html(`element.deviceId: ${element.deviceId}`)
-        return element.deviceId
-      }
-    });
-    
-  })
-  .catch(function(err) {
-    $('#log').html(err.name + ": " + err.message)
-    console.log(err.name + ": " + err.message);
-    return false
-  });
-
-  $('#log').html(`enumerateDevices() not supported.`)
-  return false
-
-}
-
-async function openCamera() {    
-  $('#log').html(` `)
-  const deviceId = listDevices();
-  let test;
-  if (!deviceId){
-    console.log(`!deviceId`)
-    $('#log').html(`no device id`)
-    test = false
-  }else{
-    test = true
-  }
-  let constraints; 
-
-if (test){
-   constraints = {
-    facingMode: "environment",
-    deviceId: deviceId
-  }
-}else{
-   constraints = {
-    facingMode: "environment",
+  try {
+    videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = videoStream;
+  } catch (err) {
+    alert("Could not access the camera");
   }
 }
 
+
+
+async function openCamera() {  
+  initializeCamera();  
   Quagga.init({
     inputStream: {
         name: "Live",
         type: "LiveStream",
         target: document.querySelector('#camera'), 
-        constraints
     },
     decoder: {
         readers: ["code_128_reader"]

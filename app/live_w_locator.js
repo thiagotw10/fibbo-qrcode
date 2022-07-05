da6f01fc0e0a45cecc16cd11f9e0307f7e65bffb
Marlus Santiago <santiago.marlus@gmail.com>
2022-07-05 13:30:53 -0300

    up

--- app/live_w_locator.js
+++ app/live_w_locator.js
@@ -1,4 +1,4 @@
-function openCamera() {
+$(function(){
     var resultCollector = Quagga.ResultCollector.create({
         capture: true,
         capacity: 20,
@@ -32,7 +32,6 @@ function openCamera() {
                 //Quagga.registerResultCollector(resultCollector);
                 App.attachListeners();
                 App.checkCapabilities();
-                Quagga.start();
             });
         },
         handleError: function(err) {
@@ -233,8 +232,6 @@ function openCamera() {
             inputStream: {
                 type : "LiveStream",
                 constraints: {
-                    width: {min: 640},
-                    height: {min: 480},
                     facingMode: "environment",
                     aspectRatio: {min: 1, max: 2}
                 }
@@ -258,30 +255,11 @@ function openCamera() {
 
     App.init();
 
-    // Quagga.onProcessed(function(result) {
-    //     var drawingCtx = Quagga.canvas.ctx.overlay,
-    //         drawingCanvas = Quagga.canvas.dom.overlay;
+})
 
-    //     if (result) {
-    //         if (result.boxes) {
-    //             drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-    //             result.boxes.filter(function (box) {
-    //                 return box !== result.box;
-    //             }).forEach(function (box) {
-    //                 Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-    //             });
-    //         }
 
-    //         if (result.box) {
-    //             Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-    //         }
-
-    //         if (result.codeResult && result.codeResult.code) {
-    //             Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-    //         }
-    //     }
-    // });
-
+function openCamera() {
+    Quagga.start();
     Quagga.onDetected(function(result) {
         var code = result.codeResult.code;
 

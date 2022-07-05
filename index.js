208f28141cf39d109bf097362b8184b1e23b3b29
Marlus Santiago <santiago.marlus@gmail.com>
2022-07-05 11:54:41 -0300

    t

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 async function initializeCamera() {
+  const constraints = {
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440,
+      },
+    },
+  };
   stopVideoStream();
   constraints.video.facingMode = useFrontCamera ? "user" : "environment";
 
